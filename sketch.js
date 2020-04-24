d7405a7386919e869be0fafc115c4e504ec46f7c
joshuadmello <58077567+joshuadmello@users.noreply.github.com>
2020-04-24 19:54:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var gameState=0;
+var playerCount,database;
+var form,player,game;
+var allPlayers;
+
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    console.log(database);
+
+    game=new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    background("white");
+
+    if(playerCount===4){
+        game.update(1);
+    }
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+   
+}
+
+
